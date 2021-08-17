bbb1f63859bf0eb096c025e4d54efd8416ab94ce
sanidhya3340 <sanidhyabhardwaj3340@gmail.com>
2021-08-17 21:14:41 +0530

    responsive navbar made

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,10 +3,10 @@ import React from 'react'
 export default function Header() {
     return (
         <div className="flex flex-row-reverse mr-24 pt-12 mb-2">
-            <div className="bg-red-text h-8 w-8  rounded-full flex items-center justify-center ml-2 shadow-md">
+            <div className="bg-red-text h-8 w-8  rounded-full flex items-center justify-center ml-2 shadow-md cursor-pointer hover:shadow-xl">
                 <img src="/assets/icon/ig.png" alt="" />
             </div>
-            <div className="bg-red-text h-8 w-8 rounded-full flex items-center justify-center ml-2 shadow-md">
+            <div className="bg-red-text h-8 w-8 rounded-full flex items-center justify-center ml-2 shadow-md cursor-pointer hover:shadow-xl">
                 <img src="/assets/icon/fb.png" alt="" className="" />
             </div>
         </div>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,9 +3,9 @@ import { Link } from 'react-router-dom';
 
 export default function Navbar() {
     return (
-        <div className="flex justify-between px-12 py-4 items-center">
+        <div className="flex flex-col md:flex-row justify-between md:px-12 py-4 items-center">
             <div className="flex items-center ">
-                <div className="mb-2 bg-red-primary px-1 py-1 rounded-lg flex items-center justify-center">
+                <div className="mb-2 bg-red-primary md:px-1 md:py-1 rounded-lg flex items-center justify-center">
                     <img
                         src="/assets/navbar/smally.png"
                         alt=""
@@ -20,7 +20,7 @@ export default function Navbar() {
                     />
                 </div>
             </div>
-            <div className="flex items-center mr-16">
+            <div className="flex items-center md:mr-16">
                 <Link to="/" className="mx-2 hover:text-red-text hover:underline">Home</Link>
                 <Link to="/" className="mx-2 hover:text-red-text hover:underline">About</Link>
                 <Link to="/" className="mx-2 hover:text-red-text hover:underline">Services</Link>
